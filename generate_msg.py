a5a6d0c66c6e80deab75f6c052e743db0b07b520
Rama Pradana <ramapradana67@hotmail.com>
2020-02-02 21:28:11 +0800

    asd

--- generate_msg.py
+++ generate_msg.py
@@ -8,11 +8,9 @@ rowLimit = '200'
 db = DatabaseConnection(env.DB_HOST, env.DB_UNAME,
                         env.DB_PASSWORD, env.DB_NAME)
 
-db.connect()
-for i in range(1000):
+for i in range(10):
     sql = """
     INSERT INTO tb_buku(`nama_buku`, `jenisbuku_id`, isbn)
     VALUES('buku', 1, 12341234)"""
     db.executeCommit(sql=sql)
     print("{}".format(i+1))
-db.close()
--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import time
 import json
 import sys
 import env
-
+import time
 
 log = Log()
 ven = Ventilator()
-task = log.getUnproceessLog()
-if(task['execute_status']):
-    if(task['data']):
-        startTime = int(round(time.time() * 1000))
-        ven.send(task['data'])
-        print("start time : {}".format(startTime))
-        sys.exit()
-else:
-    print('Error retrieving data')
+while True:
+    task = log.getUnproceessLog()
+    if(task['execute_status']):
+        if(task['data']):
+            startTime = int(round(time.time() * 1000))
+            ven.send(task['data'])
+            print("start time : {}".format(startTime))
+            sys.exit()
+        else:
+            time.sleep(1)
+    else:
+        print('Error retrieving data')
