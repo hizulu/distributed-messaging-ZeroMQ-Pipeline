eb80f7a0c1b05e8c99f644dc9e95d88d1909c4b4
Rama Pradana <ramapradana67@hotmail.com>
2020-02-04 02:35:58 +0800

    ad

--- ventilator.py
+++ ventilator.py
@@ -61,7 +61,7 @@ class Ventilator:
                         item['client_unique_id'], item['row_id'], item['table_name'], item['msg_type'])
 
                 elif(item['msg_type'] == 'UPD'):
-                    isInsideInboxQuery = "select * from tb_sync_inbox where client_unique_id={} and row_id = {} and table_name = '{}' and msg_type='UPD' and md5(query) = '{}' and unix_timestamp {}".format(
+                    isInsideInboxQuery = "select * from tb_sync_inbox where client_unique_id={} and row_id = {} and table_name = '{}' and msg_type='UPD' and md5(query) = '{}'".format(
                         item['client_unique_id'], item['row_id'], item['table_name'], hashlib.md5(item['query'].encode()).hexdigest(), item['unix_timestamp_sync'])
 
                 inbox = self.db.executeFetchAll(
