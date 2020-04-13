e3658f49661b740fffd4fb58a6b32fc70438a79d
Rama Pradana <ramapradana67@hotmail.com>
2020-04-13 18:46:04 +0800

    asd

--- sync.py
+++ sync.py
@@ -480,7 +480,7 @@ while True:
     syncThread = threading.Thread(
         target=sync.process, args=(syncInbox['data'],))
     statusThread = threading.Thread(
-        target=sync.process, args=(syncThread['data'],))
+        target=sync.process, args=(statusThread['data'],))
 
     syncThread.start()
     statusThread.start()
